cf38c422e553a01abeab36c39ea5f1f6d6795815
David Gebhard <user@MacBook-Pro-4.fritz.box>
2024-06-15 13:27:59 +0200

    Test Commit

---
+++ example.py
@@ -0,0 +1,26 @@
+############
+#---Test---#
+############
+
+name: str = 'Peter'
+
+if name == 'Peter':
+    print ('This is an English name.')
+elif name == 'David':
+    print ('This is an international name')
+elif name == 'Janine':
+    print ('This is a French name')
+else:
+    print ('Unknown Origin')
+
+def add_numbers(a: float,b: float) -> int:
+    return a + b
+
+result = add_numbers(a=1,b=2)
+
+def do_some_math():
+    sum = 1 + 2 + 3
+do_some_math()
+
+
+
